e74eb10e97b2e9cafc1b4c62afb271f9c38ceb31
Axel <axelmychro@gmail.com>
2026-01-05 13:06:25 +0700

    chore: fix typos

--- src/app.jsx
+++ src/app.jsx
@@ -9,7 +9,7 @@ export function App() {
         <header className="bg-red-400 sm:p-4">
           <a href="#">
             <h1 className="font-display border-b-2 text-center text-2xl not-sm:text-balance sm:text-4xl">
-              Krabby Patty Tebing Tinggi
+              Kraby Patty Tebing Tinggi
             </h1>
           </a>
         </header>
